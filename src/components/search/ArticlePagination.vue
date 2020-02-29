80d584d00c827fdfa5e952eddb5382a2d5a7b9c2
John Bauer <johnbauer126@gmail.com>
2020-02-29 17:24:32 -0500

    create basic pagination ui

--- src/components/search/ArticlePagination.vue
+++ src/components/search/ArticlePagination.vue
@@ -1,6 +1,24 @@
 <template>
   <div v-if="articlesData">
     {{ articlesData.totalResults }}
+    <b-pagination
+      :total="total"
+      :current.sync="current"
+      :range-before="rangeBefore"
+      :range-after="rangeAfter"
+      :order="order"
+      :size="size"
+      :simple="isSimple"
+      :rounded="isRounded"
+      :per-page="perPage"
+      :icon-prev="prevIcon"
+      :icon-next="nextIcon"
+      aria-next-label="Next page"
+      aria-previous-label="Previous page"
+      aria-page-label="Page"
+      aria-current-label="Current page"
+    >
+    </b-pagination>
   </div>
 </template>
 
@@ -9,6 +27,21 @@ import { mapState } from "vuex";
 
 export default {
   name: "ArticlePagination",
+  data() {
+    return {
+      total: 200,
+      current: 1,
+      perPage: 10,
+      rangeBefore: 3,
+      rangeAfter: 1,
+      order: "",
+      size: "",
+      isSimple: false,
+      isRounded: true,
+      prevIcon: "chevron-left",
+      nextIcon: "chevron-right"
+    };
+  },
   computed: {
     ...mapState("news", ["articlesData"])
   }
