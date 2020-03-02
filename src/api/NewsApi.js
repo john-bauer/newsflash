74a0611b68a353a41deafe60326f9384d576b795
John Bauer <johnbauer126@gmail.com>
2020-03-02 16:14:42 -0500

    creaate third api request type for no country

--- src/api/NewsApi.js
+++ src/api/NewsApi.js
@@ -3,22 +3,6 @@ import axios from "axios";
 
 const baseUrl = "https://newsapi.org/v2";
 
-/* 
-TODO: top-headlines endpoint accepts a "country" parameter, 
-while everything endpoint accepts a language parameter.
-
-Set up the following pattern:
-
-1) User selects country on frontend (default to All)
-
-2) If filter is top-headlines, pass country to api and show 
-country selection in url
-
-3) If filter is everything, match selected country to language, 
-pass language to api, and show language in url
-
-*/
-
 const NewsApi = {
   getNews(filter, keywords, sort, page, country) {
     if (filter === "top-headlines") {
@@ -32,9 +16,8 @@ const NewsApi = {
         .catch(error => {
           return error;
         });
-    } else if (filter === "everything") {
+    } else if (filter === "everything" && country !== "") {
       const languageConversion = country => {
-        /* TODO: Explore a more elegant solution for this... */
         if (country === "us") {
           return "en";
         } else if (country === "es") {
@@ -49,6 +32,17 @@ const NewsApi = {
       return axios
         .get(
           `${baseUrl}/${filter}?language=${lang}&q=${keywords}&sortBy=${sort}&page=${page}&apiKey=${process.env.VUE_APP_NEWS_API_KEY}`
+        )
+        .then(response => {
+          return response.data;
+        })
+        .catch(error => {
+          return error;
+        });
+    } else if (filter === "everything" && country === "") {
+      return axios
+        .get(
+          `${baseUrl}/${filter}?q=${keywords}&sortBy=${sort}&page=${page}&apiKey=${process.env.VUE_APP_NEWS_API_KEY}`
         )
         .then(response => {
           return response.data;
