6899c39eef0c4d84210ad6210b8b949e3ec8c2d4
John Bauer <johnbauer126@gmail.com>
2020-03-04 17:07:30 -0500

    wrap search field in container

--- src/components/home/SearchField.vue
+++ src/components/home/SearchField.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="has-text-centered">
-    <img src="@/assets/svg/01.svg" class="-logo" />
-    <p class="subtitle">
-      Explore the latest, most relevant headlines from thousands of sources.
-    </p>
-    <form @submit.prevent="handleSubmit()">
-      <b-field>
-        <b-input
-          size="is-medium"
-          v-model="keywords"
-          placeholder="Search by Keyword"
-        ></b-input>
-      </b-field>
-    </form>
+  <div class="container">
+    <div class="has-text-centered">
+      <img src="@/assets/svg/01.svg" class="-logo" />
+      <p class="subtitle">
+        Explore the latest, most relevant headlines from thousands of sources.
+      </p>
+      <form @submit.prevent="handleSubmit()">
+        <b-field>
+          <b-input
+            size="is-medium"
+            v-model="keywords"
+            placeholder="Search by Keyword"
+          ></b-input>
+        </b-field>
+      </form>
+    </div>
   </div>
 </template>
 <script>
